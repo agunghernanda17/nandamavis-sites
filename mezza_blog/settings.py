359e2a4836befb1107f08f2e87b4ba09238a097d
agunghernanda17 <hernanda1agung@gmail.com>
2020-01-19 19:12:42 +0700

    change debug to true

--- mezza_blog/settings.py
+++ mezza_blog/settings.py
@@ -94,7 +94,7 @@ USE_MODELTRANSLATION = False
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -349,6 +349,10 @@ if os.path.exists(f):
     sys.modules[module_name] = module
     exec(open(f, "rb").read())
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 ####################
 # DYNAMIC SETTINGS #
